a8751a856e86e2ec12759837dad0270914ce9d14
Toni <80543440+ToniWD@users.noreply.github.com>
2025-09-04 18:02:49 +0300

    Add initial pet-list component with mock data

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import {TestComponent} from './component/test-component/test-component';
+import {PetListComponent} from './component/pet-list/pet-list-component';
 
 export const routes: Routes = [
   {
     path: '',
     component: TestComponent
+  },
+  {
+    path: 'pet-list',
+    component: PetListComponent
   }
 ];
