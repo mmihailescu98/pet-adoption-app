97a8bd2bf0cd338e8ebebecd005bc7c240c9d418
Sava Claudiu Ionut <savaclaudiu42@gmail.com>
2025-09-29 17:04:53 +0300

    Rectified matchers path from SecurityConfig , added a WebConfig for cors to cover all controllers , and removed the cors annotation from PetController (#74)

--- backend/src/main/java/cloudflight/integra/backend/controller/PetController.java
+++ backend/src/main/java/cloudflight/integra/backend/controller/PetController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:4200")
 @RequestMapping("/api")
 @RestController()
 public class PetController {
