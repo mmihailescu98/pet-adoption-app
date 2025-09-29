8509d82d4d41356d439d2d3edcd7dd4cdf28c847
Toni <80543440+ToniWD@users.noreply.github.com>
2025-09-29 22:47:45 +0300

    Display adoption rate, total pets, total adopted pets and most popular location on dashboard

--- frontend/src/app/store/dashboard-store/dashboard.store.ts
+++ frontend/src/app/store/dashboard-store/dashboard.store.ts
@@ -10,6 +10,10 @@ export interface DashboardState {
   mostAdoptedBreeds: AdoptionStatsDTO[];
   speciesPercentages: PercentageStatsDTO[];
   breedsPercentages: PercentageStatsDTO[];
+  totalPets: number;
+  totalAdoptedPets: number;
+  adoptionRate: number;
+  mostPopularPetLocation: string;
   error: any;
   status: 'pending' | 'loading' | 'error' | 'success';
 }
@@ -19,6 +23,10 @@ const initialState: DashboardState = {
   mostAdoptedBreeds: [],
   speciesPercentages: [],
   breedsPercentages: [],
+  totalPets: -1,
+  totalAdoptedPets: -1,
+  adoptionRate: -1,
+  mostPopularPetLocation: "No location",
   error: null,
   status: 'pending'
 };
@@ -68,12 +76,57 @@ export const DashboardStore = signalStore(
           }
         },
 
+      // totalPets: number;
+      async loadTotalPets() {
+        patchState(store, { status: 'loading', error: null });
+        try {
+          const result = await lastValueFrom(dashboardService.getTotalPets());
+          patchState(store, { totalPets: result, status: 'success' });
+        } catch (err: any) {
+          patchState(store, { error: err, status: 'error' });
+        }
+      },
+      // totalAdoptedPets: number;
+      async loadTotalAdoptedPets() {
+        patchState(store, { status: 'loading', error: null });
+        try {
+          const result = await lastValueFrom(dashboardService.getTotalAdoptedPets());
+          patchState(store, { totalAdoptedPets: result, status: 'success' });
+        } catch (err: any) {
+          patchState(store, { error: err, status: 'error' });
+        }
+      },
+      // adoptionRate: number;
+      async loadAdoptionRate() {
+        patchState(store, { status: 'loading', error: null });
+        try {
+          const result = await lastValueFrom(dashboardService.getAdoptionRate());
+          patchState(store, { adoptionRate: result, status: 'success' });
+        } catch (err: any) {
+          patchState(store, { error: err, status: 'error' });
+        }
+      },
+      // mostPopularPetLocation: string;
+      async loadMostPopularLocation() {
+        patchState(store, { status: 'loading', error: null });
+        try {
+          const result = await lastValueFrom(dashboardService.mostPopularPetLocation());
+          patchState(store, { mostPopularPetLocation: result, status: 'success' });
+        } catch (err: any) {
+          patchState(store, { error: err, status: 'error' });
+        }
+      },
+
         async loadAll() {
           await Promise.all([
             this.loadMostAdoptedSpecies(),
             this.loadMostAdoptedBreeds(),
             this.loadSpeciesPercentages(),
             this.loadBreedsPercentages(),
+            this.loadTotalPets(),
+            this.loadTotalAdoptedPets(),
+            this.loadAdoptionRate(),
+            this.loadMostPopularLocation(),
           ]);
         }
       })
