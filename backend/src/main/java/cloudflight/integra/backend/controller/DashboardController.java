6a1ab4478fdbc3606dbe0c8877d16773f1f0008e
rDuKu <101511405+rDuca32@users.noreply.github.com>
2025-09-29 17:08:29 +0300

    Finished

--- backend/src/main/java/cloudflight/integra/backend/controller/DashboardController.java
+++ backend/src/main/java/cloudflight/integra/backend/controller/DashboardController.java
@@ -4,14 +4,12 @@ import cloudflight.integra.backend.dto.AdoptionStatsDTO;
 import cloudflight.integra.backend.dto.PercentageStatsDTO;
 import cloudflight.integra.backend.service.DashboardService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:4200")
 @RequestMapping("/api/dashboard")
 @RestController()
 public class DashboardController {
