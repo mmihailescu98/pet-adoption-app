47ab073ef795b3c62bf1916d44a7c20ed3216070
Sava Claudiu Ionut <savaclaudiu42@gmail.com>
2025-10-10 17:10:42 +0300

    Feature/sci/be/add pet to adoption (#69)
    * Implemented login and register
    
    * Added adoption model and related classes , with createAdoption Endpoint, changed UserRepository to use Jpa
    
    * Modified pet-list to use pets from AdoptionEntry ,
    created endpoint for getAdoptions,
    fixed the Blob thing,
    -added an "adoptedAt" attribute on AdoptionEntry to mark the moment of adoption
    
    * Added tests and other requested changes
    
    * Slight logic change , Flyway migration added
    
    ---------
    
    Co-authored-by: Darius077 <darius_george15@yahoo.com>

---
+++ backend/src/main/java/cloudflight/integra/backend/mapper/AdoptionMapper.java
@@ -0,0 +1,50 @@
+package cloudflight.integra.backend.mapper;
+
+import cloudflight.integra.backend.dto.AdoptionAddRequestDTO;
+import cloudflight.integra.backend.dto.AdoptionListItemDTO;
+import cloudflight.integra.backend.model.AdoptionEntry;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Mapper(uses = {PetMapper.class})
+public interface AdoptionMapper {
+    AdoptionMapper INSTANCE = Mappers.getMapper(AdoptionMapper.class);
+
+    /**
+     * Maps an AdoptionAddRequestDTO to an Adoption from the model ,
+     *  and afterward sets default values to missing values
+     * @param dto Source
+     * @return Target
+     */
+    @Mapping(target = "adopter", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "adoptedAt", ignore = true)
+    AdoptionEntry toModelFromAddRequest(AdoptionAddRequestDTO dto);
+
+    /**
+     * Maps an AdoptionEntry list to a AdoptionGetRequestDTO list
+     * @param models list with the model entities
+     * @return items for showcase of adoptions
+     */
+    @Mapping(target = "pet",source = "pet")//needed because it goes from model to dto
+    List<AdoptionListItemDTO> toListItemsFromModels(List<AdoptionEntry> models);
+
+
+    @Mapping(target = "pet",source = "pet")
+    AdoptionListItemDTO toListItemFromModel(AdoptionEntry model);
+
+
+    @AfterMapping
+    default void setDefaults(@MappingTarget AdoptionEntry adoption) {
+        adoption.setCreatedAt(LocalDateTime.now());
+        adoption.setAdoptedAt(null);
+        adoption.setAdopter(null);
+    }
+}
